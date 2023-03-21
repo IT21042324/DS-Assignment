4aab584663ccc2de71b63d9dfb8d41e5babaaab7
Reezan Saleem <it21042324@my.sliit.lk>
2023-03-21 23:26:15 +0530

    Added Working HomePage for Client

--- Project/frontend/src/App.js
+++ Project/frontend/src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Home from "./pages/Buyer/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
