8a0912fab7f20fb4833a096e47c088d228afb9f3
Sarindu <it21078996@my.sliit.lk>
2023-03-22 20:03:55 +0530

    Extra Banners, Footer update

--- Project/frontend/src/pages/Buyer/Home.js
+++ Project/frontend/src/pages/Buyer/Home.js
@@ -1,7 +1,10 @@
+import ActionBanner from "../../components/ActionBanner";
 import Features from "../../components/Features";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import HomeBanner from "../../components/HomeBanner";
+import MiniBanner from "../../components/MiniBanner";
+import SmallBanner from "../../components/SmallBanner";
 
 export default function Home() {
   return (
@@ -9,6 +12,9 @@ export default function Home() {
       <Header />
       <HomeBanner />
       <Features />
+      <ActionBanner />
+      <SmallBanner />
+      <MiniBanner />
       <Footer />
     </div>
   );
