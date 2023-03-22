c78604636fa1f41e8876b7244f34e5c4cad0298b
Sarindu <it21078996@my.sliit.lk>
2023-03-22 17:19:29 +0530

    Features component

--- Project/frontend/src/pages/Buyer/Home.js
+++ Project/frontend/src/pages/Buyer/Home.js
@@ -1,10 +1,14 @@
+import Features from "../../components/Features";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import HomeBanner from "../../components/HomeBanner";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <HomeBanner />
+      <Features />
       <Footer />
     </div>
   );
