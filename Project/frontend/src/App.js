d2d33ad45597ca7c6ce2e9e2c36acad9e50556dd
Sarindu <it21078996@my.sliit.lk>
2023-03-22 10:55:00 +0530

    HomeBanner component

--- Project/frontend/src/App.js
+++ Project/frontend/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeBanner from "./components/HomeBanner";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <HomeBanner />
       <Footer />
     </div>
   );
