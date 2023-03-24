2eeffcf51f9f3697fa7609ea7319bcfa77c3a489
Sarindu <it21078996@my.sliit.lk>
2023-03-25 00:44:57 +0530

    login and register forms

--- Project/frontend/src/App.js
+++ Project/frontend/src/App.js
@@ -4,6 +4,8 @@ import Cart from "./pages/Buyer/Cart";
 import { Routes, Route } from "react-router-dom";
 import Product from "./pages/Buyer/Product";
 import React from 'react';
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -12,6 +14,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Cart" element={<Cart />} />
         <Route path="/product" element={<Product />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/seller/register" element={<Register />} />
       </Routes>
     </div>
   );
