98421257ace59b4771f3e2a9e3c96476e5253502
Sarindu <it21078996@my.sliit.lk>
2023-04-09 20:03:43 +0530

    app.js changest

--- Project/frontend/src/App.js
+++ Project/frontend/src/App.js
@@ -1,38 +1,3 @@
-// import "./App.css";
-// import Home from "./pages/Buyer/Home";
-// import { Routes, Route, Navigate } from "react-router-dom";
-// import Login from "./components/Login";
-// import Register from "./components/Register";
-// import { UseUserContext } from "./context/useUserContext";
-// import { BuyerRoutes } from "./BuyerRoutes";
-// import { SellerRoutes } from "./SellerRoutes";
-
-// export default function App() {
-//   const { user1 } = UseUserContext();
-
-//   return (
-//     <div className="App">
-//       <Routes>
-//         <Route
-//           path="/"
-//           element={
-//             !user1[0]?.role === "Buyer" ? <Home /> : <Navigate to="/seller" />
-//           }
-//         />
-
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/register" element={<Register />} />
-
-//         {(user1[0]?.role === "Buyer" || !user1[0]) && (
-//           <Route path="/buyer/*" element={<BuyerRoutes />} />
-//         )}
-//         {user1[0]?.role === "Merchant" && (
-//           <Route path="/seller/*" element={<SellerRoutes />} />
-//         )}
-//       </Routes>
-//     </div>
-//   );
-// }
 import "./App.css";
 import Home from "./pages/Buyer/Home";
 import { Routes, Route, Navigate } from "react-router-dom";
